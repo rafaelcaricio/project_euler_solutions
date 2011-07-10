8c480ed17961f5d6339c9b0a80cd28c1b4d2af99
Rafael Caricio <rafael.jacinto@gmail.com>
2011-07-10 13:46:12 -0300

    Solution for problem 3. Move on.

---
+++ problem_3/solution.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int is_prime(long number) {
+    long n;
+    if (number <= 1) {
+        return 0;
+    }
+
+    for (n = number; n > 1; n--) {
+        if (number % n == 0 && number != n) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(int argc, char* argv) {
+    long num = 600851475143, i = 0;
+
+    while (num != 1) {
+        if (is_prime(i)) {
+            if (num % i == 0) {
+                printf("%ld\n", i);
+                num = num / i;
+            }
+        }
+        i++;
+    }
+
+    return 0;
+}
